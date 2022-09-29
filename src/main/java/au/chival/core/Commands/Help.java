4f26942039c28b3a144f7e6b6f37c717592b7633
BuildTools <unconfigured@null.spigotmc.org>
2022-09-29 21:09:35 +1300

    aaaaaaaaaaaaaaaaaaaaa

--- src/main/java/au/chival/core/Commands/Help.java
+++ src/main/java/au/chival/core/Commands/Help.java
@@ -8,7 +8,7 @@ import org.bukkit.entity.Player;
 public class Help implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
-
+            commandSender.sendMessage("Currently no help. or as jack whitehall said 'YOURE ON YOUR OWN MATEEE'");
         return true;
     }
 }
